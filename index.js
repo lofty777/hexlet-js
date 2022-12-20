95ce58c4cf3e4381b2c641494772f185c67cffc8
Aleksandr Meshkov <meshkov88@gmail.com>
2022-12-21 06:12:20 +0700

    Install prettier and jest

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-console.log(_.intersection([2,1], [2,3]));
+console.log(_.intersection([2, 1], [2, 3]));
 console.log(_.capitalize("hello"));
-console.log(_.last(['one', 'two']));
-
-
+console.log(_.last(["one", "two"]));
