c47a3b62966cf23fa11f64dc96dae3fc83820d29
Aleksandr Meshkov <meshkov88@gmail.com>
2022-12-21 07:10:48 +0700

    Install eslint

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import _ from "lodash";
+import _ from 'lodash';
 
 console.log(_.intersection([2, 1], [2, 3]));
-console.log(_.capitalize("hello"));
-console.log(_.last(["one", "two"]));
+console.log(_.capitalize('hello'));
+console.log(_.last(['one', 'two']));
+
+const findSum = (a, b) => {
+  const c = a + b;
+  return c;
+};
+console.log(findSum(4, 55));
